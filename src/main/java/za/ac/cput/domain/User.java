1d7f37d2059445df9d19b98e5af67c923c567710
Alungile-Maya <gqutyana22@gmail.com>
2025-05-11 23:21:06 +0200

    user

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -1,4 +1,141 @@
 package za.ac.cput.domain;
 
-public class User {
+public class User{
+
+    private  String firstName;
+    private String lastName;
+    private int identificationNumber;
+    private  long cellPhone;
+    private String email;
+    private String userId;
+    private String password;
+    private String currentLocation;
+    private String notificationPreference;
+
+    private User(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public long getCellPhone() {
+        return cellPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public String getNotificationPreference() {
+        return notificationPreference;
+    }
+    public User(Builder builder){
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.identificationNumber = builder.identificationNumber;
+        this.cellPhone = builder.cellPhone;
+        this.email = builder.email;
+        this.userId = builder.userId;
+        this.password = builder.password;
+        this.currentLocation = builder.currentLocation;
+        this.notificationPreference = builder.notificationPreference;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", identificationNumber=" + identificationNumber +
+                ", cellPhone=" + cellPhone +
+                ", email='" + email + '\'' +
+                ", userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", currentLocation='" + currentLocation + '\'' +
+                ", notificationPreference='" + notificationPreference + '\'' +
+                '}';
+    }
+    public static class Builder{
+        private  String firstName;
+        private String lastName;
+        private int identificationNumber;
+        private  long cellPhone;
+        private String email;
+        private String userId;
+        private String password;
+        private String currentLocation;
+        private String notificationPreference;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setIdentificationNumber(int identificationNumber) {
+            this.identificationNumber = identificationNumber;
+            return this;
+        }
+
+        public Builder setCellPhone(long cellPhone) {
+            this.cellPhone = cellPhone;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public  Builder setCurrentLocation(String currentLocation) {
+            this.currentLocation = currentLocation;
+            return this;
+        }
+
+        public Builder  setNotificationPreference(String notificationPreference) {
+            this.notificationPreference = notificationPreference;
+            return this;
+        }
+        public User build(){
+            return new User(this);
+        }
+
+
+    }
 }
