16d8203acd9b84b95a396517acee79847399d482
Anele77085 <anelejaji03@gmail.com>
2025-08-18 23:32:17 +0200

    updated the interphase of Customer Service Class

--- src/main/java/za/ac/cput/service/ICustomerService.java
+++ src/main/java/za/ac/cput/service/ICustomerService.java
@@ -1,10 +1,10 @@
 package za.ac.cput.service;
 
 import za.ac.cput.domain.Customer;
+import za.ac.cput.domain.FullWash;
+
 import java.util.List;
 
 public interface ICustomerService extends IService<Customer, Long> {
-    List<Customer> findByAddressId(Long id);
     List<Customer> getAll();
-    List<Customer> findAll();// fixed method name
 }
