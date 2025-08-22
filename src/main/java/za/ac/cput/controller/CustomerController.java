db46d1b48b1b66fbcb917c8c403be6c8c8b06a1f
Anele77085 <anelejaji03@gmail.com>
2025-08-22 19:50:24 +0200

    updated the Customer Controller class

--- src/main/java/za/ac/cput/controller/CustomerController.java
+++ src/main/java/za/ac/cput/controller/CustomerController.java
@@ -1,4 +1,44 @@
 package za.ac.cput.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.service.CustomerService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    private CustomerService service;
+
+    @Autowired
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+@PostMapping("/create")
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return service.create(customer);
+}
+
+@GetMapping("/read/{userId}")
+    public Customer readCustomer(@PathVariable String userId) {
+        return service.read(userId);
+}
+
+@PutMapping("/update")
+    public Customer updateCustomer(@RequestBody Customer customer) {
+        return service.update(customer);
+}
+
+@RequestMapping("/delete/{userId}")
+    public boolean deleteCustomer(@PathVariable String userId) {
+        return service.delete(userId);
+}
+
+@GetMapping("/getall")
+    public List<Customer> getAllCustomers() {
+        return service.getAll();
+}
 }
